bf2b53a85974ac1b3e00b3828ee51b6a7a4546cf
inji <inji@192.168.0.88>
2015-06-30 13:09:23 +0700

    cache control

--- system/modules/StaticLoader/StaticLoader.php
+++ system/modules/StaticLoader/StaticLoader.php
@@ -164,7 +164,7 @@ class StaticLoader extends Module {
         header("Pragma: public");
         header("Content-Length: " . filesize($file));
         header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($file)) . ' GMT');
-        if (filemtime($file) <= $modifiedSince && (isset($_SERVER['HTTP_CACHE_CONTROL']) && $_SERVER['HTTP_CACHE_CONTROL'] != 'no-cache')) {
+        if (filemtime($file) <= $modifiedSince && (!isset($_SERVER['HTTP_CACHE_CONTROL']) || $_SERVER['HTTP_CACHE_CONTROL'] != 'no-cache')) {
             // Разгружаем канал передачи данных!
             header('HTTP/1.1 304 Not Modified');
             exit();
