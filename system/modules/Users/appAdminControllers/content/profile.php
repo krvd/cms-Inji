3d065b4da53b009632d442253fc33f2271b49624
inji <inji@192.168.0.88>
2015-07-05 21:33:46 +0700

    fix

--- system/modules/Users/appAdminControllers/content/profile.php
+++ system/modules/Users/appAdminControllers/content/profile.php
@@ -1,9 +1,9 @@
       
 <div class="text-center">
     <h3><?php echo Users\User::$cur->user_login; ?></h3>
-    <img  style="padding:2%;" src="http://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/twDq00QDud4/s120-c/photo.jpg"/>
+    <img  style="padding:2%;" src="/static/system/images/no-photo.png"/>
     <br />
     <a href ="/admin" class = 'btn btn-primary'>Перейти в админ панель</a>
-    <a href ="/admin?logout=1" class = 'btn btn-danger'>Выйи</a>
+    <a href ="/admin?logout=1" class = 'btn btn-danger'>Выйти</a>
 
 </div>
