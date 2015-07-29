27d2408c4c59b903096c86dd57a5594530925df8
inji <inji@admin.mlmsystem.local>
2015-07-29 17:27:52 +0700

    number input, ecommerce app controlelr

---
+++ system/modules/Ui/widgets/Form/number.php
@@ -0,0 +1,10 @@
+
+<?php
+echo empty($options['noContainer']) ? '<div class="form-group">' : '';
+echo $label !== false ? "<label>{$label}</label>" : '';
+?>
+<input <?= !empty($options['required']) ? 'required' : ''; ?> type ="number" placeholder="<?= !empty($options['placeholder']) ? $options['placeholder'] : ''; ?>" class="form-control" name = '<?= $name; ?>' value = '<?= !empty($options['value']) ? addcslashes($options['value'], "'") : (!empty($form->userDataTree[$name]) ? addcslashes($form->userDataTree[$name], "'") : ''); ?>' />
+<?php
+echo!empty($options['helpText']) ? "<div class='help-block'>{$options['helpText']}</div>" : '';
+echo empty($options['noContainer']) ? '</div>' : '';
+?>
