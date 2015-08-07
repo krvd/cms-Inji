b774e0b4c520d999b8dea5d0d07cad3c6419578f
inji <inji@inji-pc>
2015-08-07 13:12:33 +0700

    hidden fix

--- system/modules/Ui/objects/ActiveForm/Input/Hidden.php
+++ system/modules/Ui/objects/ActiveForm/Input/Hidden.php
@@ -1,1 +1,16 @@
-<input type ="hidden" name = '<?= $name; ?>' value = '<?= !empty($options['value']) ? addcslashes($options['value'], "'") : ''; ?>' />
+<?php
+
+/**
+ * Hidden input
+ *
+ * @author Alexey Krupskiy <admin@inji.ru>
+ * @link http://inji.ru/
+ * @copyright 2015 Alexey Krupskiy
+ * @license https://github.com/injitools/cms-Inji/blob/master/LICENSE
+ */
+
+namespace Ui\ActiveForm\Input;
+
+class Hidden extends \Ui\ActiveForm\Input {
+    
+}
