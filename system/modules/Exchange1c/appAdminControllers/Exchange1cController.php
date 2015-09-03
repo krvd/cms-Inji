3000b065e0c4ef68d0b87e99a0adbb005048969e
inji <inji@inji-pc>
2015-09-03 11:51:26 +0700

    add method for remigrate exchange

--- system/modules/Exchange1c/appAdminControllers/Exchange1cController.php
+++ system/modules/Exchange1c/appAdminControllers/Exchange1cController.php
@@ -10,12 +10,53 @@
  * @copyright 2015 Alexey Krupskiy
  * @license https://github.com/injitools/cms-Inji/blob/master/LICENSE
  */
-
-/**
- * Description of Exchange1cController
- *
- * @author inji
- */
 class Exchange1cController extends adminController {
-    //put your code here
+
+    function reExchangeAction() {
+
+        $reExchange = Exchange1c\Exchange::get((int) $_GET['item_pk']);
+
+        $exchange = new \Exchange1c\Exchange();
+        $exchange->type = $reExchange->type;
+        $exchange->save();
+        $exchange->path = App::$primary->path . '/tmp/Exchange1c/' . date('Y-m-d') . '/' . $exchange->id;
+        Tools::createDir($exchange->path);
+        $exchange->save();
+
+        foreach ($reExchange->files as $reFile) {
+            if (strpos($reFile->name, '/')) {
+                Tools::createDir($exchange->path . '/' . substr($reFile->name, 0, strrpos($reFile->name, '/')));
+            }
+            copy($reExchange->path . '/' . $reFile->name, $exchange->path . '/' . $reFile->name);
+        }
+        foreach ($reExchange->logs as $reLog) {
+            if (!in_array($reLog->info, ['import'])) {
+                continue;
+            }
+            $_GET = json_decode($reLog->query,true);
+
+            $log = new \Exchange1c\Exchange\Log();
+            $log->exchange_id = $exchange->id;
+            $log->type = 'mode';
+            $log->info = $reLog->info;
+            $log->status = 'process';
+            $log->query = $reLog->query;
+            $log->save();
+
+
+            $modeClass = 'Exchange1c\Mode\\' . ucfirst(strtolower($log->info));
+            if (!class_exists($modeClass)) {
+                $log->status = 'failure';
+                $log->info = 'mode class ' . $modeClass . ' not found';
+                $log->date_end = date('Y-m-d H:i:s');
+                $log->save();
+            }
+            $mode = new $modeClass;
+            $mode->exchange = $exchange;
+            $mode->log = $log;
+            $mode->process();
+        }
+        Tools::redirect('/admin/exchange1c/Exchange');
+    }
+
 }
