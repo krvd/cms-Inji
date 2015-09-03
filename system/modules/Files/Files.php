fb3678f7d004a383fb81787f2387467737b90996
inji <inji@inji-pc>
2015-09-03 11:52:27 +0700

    upload from url now have type filters and upload code

--- system/modules/Files/Files.php
+++ system/modules/Files/Files.php
@@ -36,8 +36,7 @@ class Files extends Module {
                 $fileObject = new Files\File();
                 $fileObject->code = $options['file_code'];
                 $fileObject->name = microtime(true);
-            }
-            elseif(!$fileObject->name){
+            } elseif (!$fileObject->name) {
                 $fileObject->name = microtime(true);
             }
         } else {
@@ -70,24 +69,9 @@ class Files extends Module {
      * --	[allow_types]: досупные для заливки типы файлов. Например image (тип форматов из таблицы типов файлов file_type_ext)
      */
     function uploadFromUrl($url, $options = array()) {
-        $fileinfo = pathinfo($url);
-        /*
-          'dirname' => string '/userfiles' (length=10)
-          'basename' => string 'avtokran(1).gif' (length=15)
-          'extension' => string 'gif' (length=3)
-          'filename' => string 'avtokran(1)' (length=11)
-         */
-
         $site_path = App::$primary->path;
-        Tools::createDir($site_path . '/static/tmp/');
-        $file = @file_get_contents($url);
-        if ($file === false) {
-            return 0;
-        }
-        file_put_contents($site_path . '/static/tmp/' . $fileinfo['basename'], $file);
-        /* if (!copy($url, App::$cur->app['path'] . '/static/tmp/' . $fileinfo['basename']))
-          return false; */
 
+        $fileinfo = pathinfo($url);
         if (empty($fileinfo['extension']))
             return 0;
 
@@ -95,26 +79,39 @@ class Files extends Module {
         if (!$type)
             return 0;
 
+        if (!empty($options['accept_group']) && $options['accept_group'] != $type->group) {
+            return 0;
+        }
+
         $fileObject = new Files\File();
         if (!empty($options['file_code'])) {
-            $fileObject = Files\File::get($options['file_code'], 'file_code');
+            $fileObject = Files\File::get($options['file_code'], 'code');
+            if (!$fileObject) {
+                $fileObject = new Files\File();
+                $fileObject->code = $options['file_code'];
+                $fileObject->name = microtime(true);
+            } elseif (!$fileObject->name) {
+                $fileObject->name = microtime(true);
+            }
         } else {
             $fileObject->name = microtime(true);
         }
 
         $fileObject->path = $type->type_dir . date('Y-m-d') . '/' . $fileObject->name . '.' . $fileinfo['extension'];
-        if (!empty($options['file_code']) && file_exists($fileObject->path))
-            unlink($path);
+        if ($fileObject->id && file_exists($site_path . $fileObject->path))
+            unlink($site_path . $fileObject->path);
 
         Tools::createDir($site_path . $type->type_dir . date('Y-m-d') . '/');
 
-        if (!copy($site_path . '/static/tmp/' . $fileinfo['basename'], $site_path . $fileObject->path))
-            return false;
-        unlink($site_path . '/static/tmp/' . $fileinfo['basename']);
+        $file = @file_get_contents($url);
+        if ($file === false) {
+            return 0;
+        }
+        file_put_contents($site_path . $fileObject->path, $file);
 
         $fileObject->type_id = $type->pk();
         $fileObject->original_name = $fileinfo['basename'];
-        $fileObject->date_create = 'CURRENT_TIMESTAMP';
+        $fileObject->upload_code = !empty($options['upload_code']) ? $options['upload_code'] : 'untracked';
         $fileObject->save();
 
         return $fileObject->id;
