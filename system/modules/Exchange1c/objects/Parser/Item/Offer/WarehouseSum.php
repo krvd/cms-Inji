bb7ff72b6312e3cc4e8f8d5d043b81a91a93dc27
inji <inji@inji-pc>
2015-09-04 12:45:37 +0700

    add single attribute warehouse parser

---
+++ system/modules/Exchange1c/objects/Parser/Item/Offer/WarehouseSum.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Item name
+ *
+ * Info
+ *
+ * @author Alexey Krupskiy <admin@inji.ru>
+ * @link http://inji.ru/
+ * @copyright 2015 Alexey Krupskiy
+ * @license https://github.com/injitools/cms-Inji/blob/master/LICENSE
+ */
+
+namespace Exchange1c\Parser\Item\Offer;
+
+class WarehouseSum extends \Migrations\Parser {
+
+    function parse() {
+        $count = (string) $this->reader;
+        $warehouse = \Ecommerce\Warehouse::get(['name','Общий склад 1с']);
+        if (!$warehouse) {
+            $warehouse = new \Ecommerce\Warehouse();
+            $warehouse->name = 'Общий склад 1с';
+            $warehouse->save();
+        }
+        $modelName = get_class($this->object->model);
+
+        $warehouseOffer = \Ecommerce\Item\Offer\Warehouse::get([
+                    [$modelName::index(), $this->object->model->pk()],
+                    [\Ecommerce\Warehouse::index(), $warehouse->id]
+        ]);
+        if (!$warehouseOffer) {
+            $warehouseOffer = new \Ecommerce\Item\Offer\Warehouse([
+                $modelName::index() => $this->object->model->pk(),
+                \Ecommerce\Warehouse::index() => $warehouse->id,
+                'count' => $count
+            ]);
+        } else {
+            $warehouseOffer->count = $count;
+        }
+        $warehouseOffer->save();
+    }
+
+}
