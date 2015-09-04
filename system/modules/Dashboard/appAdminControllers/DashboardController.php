eedc452067201cdcf57891468c34a5ac5a820f22
inji <inji@inji-pc>
2015-09-04 15:05:47 +0700

    dashboard as system module

---
+++ system/modules/Dashboard/appAdminControllers/DashboardController.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Dashboard controller
+ *
+ * @author Alexey Krupskiy <admin@inji.ru>
+ * @link http://inji.ru/
+ * @copyright 2015 Alexey Krupskiy
+ * @license https://github.com/injitools/cms-Inji/blob/master/LICENSE
+ */
+class DashboardController extends adminController {
+
+    function indexAction() {
+        $this->view->setTitle('Панель управления');
+        $this->view->page();
+    }
+
+    function siteConfigAction() {
+        if (isset($_POST['site_name'])) {
+            $config = \App::$primary->config;
+            $config['site']['name'] = $_POST['site_name'];
+            $config['site']['email'] = $_POST['site_email'];
+            $config['site']['keywords'] = $_POST['site_keywords'];
+            $config['site']['description'] = $_POST['site_description'];
+            $config['site']['metatags'] = $_POST['metatags'];
+            if (!empty($_FILES['site_logo']['tmp_name'])) {
+                $fileId = $this->Files->upload($_FILES['site_logo'], array('file_code' => 'site_logo'));
+                $config['site']['site_logo'] = Files\File::get($fileId)->path;
+            }
+            Config::save('app', $config);
+            Tools::redirect('/admin/dashboard/siteConfig', 'Изменения сохранены', 'success');
+        }
+
+        $this->view->setTitle('Общие настройки сайта');
+        $this->view->page();
+    }
+
+}
---
+++ system/modules/Dashboard/models/Comment.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Item name
+ *
+ * Info
+ *
+ * @author Alexey Krupskiy <admin@inji.ru>
+ * @link http://inji.ru/
+ * @copyright 2015 Alexey Krupskiy
+ * @license https://github.com/injitools/cms-Inji/blob/master/LICENSE
+ */
+
+namespace Dashboard;
+
+class Comment extends \Model {
+
+    static $objectName = 'Комментарии';
+    static $labels = [
+        'model' => 'Тип ресурса',
+        'item_id' => 'Ресурс',
+        'user_id' => 'Автор',
+        'text' => 'Текст',
+        'date' => 'Дата',
+    ];
+    static $cols = [
+        'user_id' => [
+            'type' => 'relation',
+            'relation' => 'user',
+            'showCol' => 'mail'
+        ],
+        'item_id' => [
+            'type' => 'void',
+            'value' => [
+                'type' => 'moduleMethod',
+                'module' => 'Dashboard',
+                'method' => 'itemHref'
+            ]
+        ]
+    ];
+    static $dataManagers = [
+        'manager' => [
+            'options' => [
+                'access' => [
+                    'groups' => [
+                        3, 4
+                    ]
+                ],
+                'formOnPage' => true
+            ],
+            'cols' => [
+                'model',
+                'item_id',
+                'user_id',
+                'text',
+                'date'
+            ],
+            'sortable' => [
+                'user_id',
+                'text',
+                'date'
+            ],
+            'preSort' => [
+                'date' => 'desc'
+            ],
+            'rowButtons' => [
+                'open'
+            ]
+        ],
+    ];
+
+    static function relations() {
+        return [
+            'user' => [
+                'model' => 'Users\User',
+                'col' => 'user_id'
+            ]
+        ];
+    }
+
+}
