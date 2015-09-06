c638e201ca95e45e7bf3a83d796a92aeaf682d16
inji <inji@inji-pc>
2015-09-06 15:47:58 +0700

    remove empty menu items

--- system/modules/Exchange1c/info.php
+++ system/modules/Exchange1c/info.php
@@ -6,21 +6,8 @@ return [
         'appAdmin' => [
             [
                 'name' => 'Обмен с 1с',
-                'href' => '/admin/exchange1c',
-                'childs' => [
-                    [
-                        'name' => 'История',
-                        'href' => '/admin/exchange1c/exchange'
-                    ],
-                    [
-                        'name' => 'Ручной обмен',
-                        'href' => '/admin/exchange1c/manual'
-                    ],
-                    [
-                        'name' => 'Конфигурация',
-                        'href' => '/admin/exchange1c/configure'
-                    ]
-                ]
+                'href' => '/admin/exchange1c/exchange',
+                'childs' => []
             ]
         ]
     ]
