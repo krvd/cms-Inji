4ad7f1f98ede951bfe58056743dda1525981d837
inji <inji@inji-pc>
2015-09-06 18:17:29 +0700

    widgets module

---
+++ system/modules/Widgets/Widgets.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Item name
+ *
+ * Info
+ *
+ * @author Alexey Krupskiy <admin@inji.ru>
+ * @link http://inji.ru/
+ * @copyright 2015 Alexey Krupskiy
+ * @license https://github.com/injitools/cms-Inji/blob/master/LICENSE
+ */
+
+/**
+ * Description of Цшвпуеы
+ *
+ * @author inji
+ */
+class Widgets extends \Module {
+    //put your code here
+}
---
+++ system/modules/Widgets/appAdminControllers/WidgetsController.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * Item name
+ *
+ * Info
+ *
+ * @author Alexey Krupskiy <admin@inji.ru>
+ * @link http://inji.ru/
+ * @copyright 2015 Alexey Krupskiy
+ * @license https://github.com/injitools/cms-Inji/blob/master/LICENSE
+ */
+
+/**
+ * Description of WidgetsController
+ *
+ * @author inji
+ */
+class WidgetsController extends Controller {
+
+    function widgetChooserAction() {
+        $widgets = [];
+        foreach (App::$primary->config['modules'] as $module) {
+            $info = Module::getInfo($module);
+            if (!empty($info['widgets'])) {
+                $widgets += $info['widgets'];
+            }
+        }
+        $this->view->page(['page' => 'blank', 'data' => compact('widgets')]);
+    }
+
+    function widgetImageAction() {
+        if (!empty($_GET['text'])) {
+            $widgetCode = explode(':', preg_replace('!^{WIDGET:!isU', '', preg_replace('!}$!isU', '', urldecode($_GET['text']))));
+            $text = 'Виджет: ';
+            $widget = false;//Widget::get($widgetCode[0], 'widget_filename');
+
+            if ($widget) {
+                $text .= $widget->widget_name . "\n";
+                $i = 1;
+                if (isset($widgetCode[$i]) && $widget->widget_params) {
+                    $params = json_decode($widget->widget_params, true);
+                    if ($params) {
+                        foreach ($params as $param) {
+                            if (!isset($widgetCode[$i]))
+                                break;
+                            if ($param['type'] == 'select') {
+                                $item = $param['model']::get($widgetCode[$i++]);
+                                if ($item) {
+                                    $text .= $param['name'] . ': ' . $item->$param['showCol'] . "\n";
+                                } else {
+                                    $text .= $widgetCode[$i - 1];
+                                }
+                            } else {
+                                $value = $widgetCode[$i++];
+                                if (mb_strlen($value, 'utf-8') > 50) {
+                                    $value = mb_substr($value, 0, 50) . '...';
+                                }
+                                $text .= $param['name'] . ': ' . $value . "\n";
+                            }
+                        }
+                    }
+                } else {
+                    unset($widgetCode[0]);
+                    foreach ($widgetCode as $item) {
+                        $text .= $item . "\n";
+                    }
+                }
+            } else {
+                foreach ($widgetCode as $item) {
+                    $text .= $item . "\n";
+                }
+            }
+        } else {
+            $text = 'text not defined';
+        }
+
+        $textWidth = mb_strlen($text, 'utf-8');
+        header('Content-type: image/png');
+        // шрифт
+        $font = dirname(__FILE__) . '/../fonts/Cousine/Cousine-Regular.ttf';
+        // вычисляем сколько места займёт текст
+        $bbox = imageftbbox(10, 0, $font, $text);
+
+
+
+
+        $width = abs($bbox[0]) + abs($bbox[2]); // distance from left to right
+        $height = abs($bbox[1]) + abs($bbox[5]); // distance from top to bottom
+        // размер изображения
+        $img = imagecreatetruecolor($width + 10, $height + 10);
+        // цвет текста
+        $black = imagecolorallocate($img, 255, 255, 255);
+
+        // цвет фона
+        $bg = imagecolorallocate($img, 85, 101, 115);
+        imagefilledrectangle($img, 0, 0, $width + 10, $height + 10, $bg);
+
+        // вычисляем координаты для центрирования
+        $x = (imagesx($img) - $bbox[4]) / 2;
+        $y = (imagesy($img) - $bbox[5] - $bbox[3]) / 2;
+
+        // добавляем текст на изображение
+        imagefttext($img, 10, 0, $x, $y, $black, $font, $text);
+
+        // выводим изображение
+        imagepng($img);
+        // освобождаем память
+        imagedestroy($img);
+    }
+
+}
