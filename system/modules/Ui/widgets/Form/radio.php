fff267fe93ab00d83d31804a5fe3afe3356c5c86
inji <inji@inji-pc>
2015-09-10 18:19:13 +0700

    add attributes

--- system/modules/Ui/widgets/Form/radio.php
+++ system/modules/Ui/widgets/Form/radio.php
@@ -1,9 +1,22 @@
 <?php
+
 echo empty($options['noContainer']) ? '<div class="radio">' : '';
 echo $label !== false ? "<label>" : '';
-?>
-<input <?= !empty($options['disabled']) ? 'disabled="disabled"' : ''; ?> type ="radio" name = '<?= $name; ?>' value = '<?= $options['value']; ?>' <?= !empty($options['checked']) ? 'checked' : ''; ?> />
-<?php
+$attributes = [
+    'type' => 'radio',
+    'name' => $name,
+    'value' => $options['value']
+];
+if (!empty($options['disabled'])) {
+    $attributes['disabled'] = 'disabled';
+}
+if (!empty($options['checked'])) {
+    $attributes['checked'] = 'checked';
+}
+if (!empty($options['attributes'])) {
+    $attributes = array_merge($attributes, $options['attributes']);
+}
+echo Html::el('input', $attributes, '', null);
 echo $label !== false ? " {$label}</label>" : '';
 echo!empty($options['helpText']) ? "<div class='help-block'>{$options['helpText']}</div>" : '';
 echo empty($options['noContainer']) ? '</div>' : '';
