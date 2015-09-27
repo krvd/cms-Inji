0bcf4b806a0f7f096b92432f2dca51d884f18655
inji <inji@inji-pc>
2015-09-27 22:22:05 +0700

    add menu

--- system/modules/Materials/info.php
+++ system/modules/Materials/info.php
@@ -2,4 +2,13 @@
 
 return array(
     'name' => 'Материалы сайта',
+    'menu' => [
+        'appAdmin' => [
+            [
+                'name' => 'Материалы',
+                'href' => '/admin/materials',
+                'childs' => []
+            ]
+        ]
+    ]
 );
