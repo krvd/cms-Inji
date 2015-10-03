b493adc52691e0fd9e454203588a1ed0f2a67c93
inji <inji@inji-pc>
2015-10-03 11:12:47 +0700

    config fix

--- system/modules/Ecommerce/appAdminControllers/content/configure.php
+++ system/modules/Ecommerce/appAdminControllers/content/configure.php
@@ -1,41 +1,42 @@
 <h1>Настройки магазина</h1>
 <!-- Nav tabs -->
 <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Настройки</a></li>
-    <?php
-    $dataManagers = [];
-    foreach ($managers as $manager) {
-        $dataManager = new Ui\DataManager($manager);
-        $dataManagers[$manager] = $dataManager;
-        $code = 'tab_' . str_replace('\\', '_', $manager);
-        echo "<li role='presentation'><a href='#{$code}' aria-controls='{$code}' role='tab' data-toggle='tab'>{$dataManager->name}</a></li>";
-    }
-    ?>
+  <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Настройки</a></li>
+  <?php
+  $dataManagers = [];
+  foreach ($managers as $manager) {
+      $dataManager = new Ui\DataManager($manager);
+      $dataManagers[$manager] = $dataManager;
+      $code = 'tab_' . str_replace('\\', '_', $manager);
+      echo "<li role='presentation'><a href='#{$code}' aria-controls='{$code}' role='tab' data-toggle='tab'>{$dataManager->name}</a></li>";
+  }
+  ?>
 </ul>
 <div class="tab-content">
-    <div role="tabpanel" class="tab-pane fade in active" id="home">
-        <?php
-        $form = new Ui\Form();
-        $form->begin();
-        $form->input('checkbox', 'config[view_empty_warehouse]', 'Показывать отсутствующие товары', ['value' => App::$cur->ecommerce->config['view_empty_warehouse']]);
-        $form->input('checkbox', 'config[view_empty_image]', 'Показывать товары без изображения', ['value' => App::$cur->ecommerce->config['view_empty_image']]);
-        $form->input('checkbox', 'config[sell_empty_warehouse]', 'Продавать отсутствующие товары', ['value' => App::$cur->ecommerce->config['sell_empty_warehouse']]);
-        $form->input('checkbox', 'config[sell_over_warehouse]', 'Продавать сверх остатоков на складе', ['value' => App::$cur->ecommerce->config['sell_over_warehouse']]);
-        $form->end('Сохранить');
-        ?>
-    </div>
+  <div role="tabpanel" class="tab-pane fade in active" id="home">
     <?php
-    foreach ($dataManagers as $manager => $dataManager) {
-        $code = 'tab_' . str_replace('\\', '_', $manager);
-        ?>
-        <div role="tabpanel" class="tab-pane fade" id="<?= $code; ?>">
-            <?php
-            $dataManager->draw();
-            ?>
-        </div>
-        <?php
-    }
+    $form = new Ui\Form();
+    $form->begin();
+    $form->input('checkbox', 'config[view_empty_warehouse]', 'Показывать отсутствующие товары', ['value' => App::$cur->ecommerce->config['view_empty_warehouse']]);
+    $form->input('checkbox', 'config[view_empty_image]', 'Показывать товары без изображения', ['value' => App::$cur->ecommerce->config['view_empty_image']]);
+    $form->input('checkbox', 'config[sell_empty_warehouse]', 'Продавать отсутствующие товары', ['value' => App::$cur->ecommerce->config['sell_empty_warehouse']]);
+    $form->input('checkbox', 'config[sell_over_warehouse]', 'Продавать сверх остатоков на складе', ['value' => App::$cur->ecommerce->config['sell_over_warehouse']]);
+    $form->input('hidden', 'config[save]', '', ['value' => 1]);
+    $form->end('Сохранить');
     ?>
+  </div>
+  <?php
+  foreach ($dataManagers as $manager => $dataManager) {
+      $code = 'tab_' . str_replace('\\', '_', $manager);
+      ?>
+      <div role="tabpanel" class="tab-pane fade" id="<?= $code; ?>">
+        <?php
+        $dataManager->draw();
+        ?>
+      </div>
+      <?php
+  }
+  ?>
 </div>
 
 
