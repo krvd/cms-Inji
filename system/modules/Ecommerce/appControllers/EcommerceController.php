181b162ebb60a45225f594de36b037b746e19747
Alexey Krupsky <admin@inji.ru>
2015-11-03 19:42:02 +0700

    bread reverse fix

--- system/modules/Ecommerce/appControllers/EcommerceController.php
+++ system/modules/Ecommerce/appControllers/EcommerceController.php
@@ -179,7 +179,6 @@ class ecommerceController extends Controller
         $bread[] = array('text' => 'Каталог', 'href' => '/ecommerce');
 
         $catalogIds = array_values(array_filter(explode('/', $item->tree_path)));
-        $catalogIds = array_reverse($catalogIds);
         foreach ($catalogIds as $id) {
             $cat = Ecommerce\Category::get($id);
             $bread[] = array('text' => $cat->name, 'href' => '/ecommerce/itemList/' . $cat->id);
