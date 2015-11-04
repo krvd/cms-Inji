dfb079614da969afeb039ced02fed55874d1b7aa
Alexey Krupsky <admin@inji.ru>
2015-11-04 18:49:32 +0700

    remove notifications from menu

--- system/modules/Notifications/info.php
+++ system/modules/Notifications/info.php
@@ -3,13 +3,4 @@
 return array(
     'name' => 'Уведомления',
     'autoload' => true,
-    'menu' => [
-        'appAdmin' => [
-            [
-                'name' => 'Уведомления',
-                'href' => '/admin/notifications',
-                'childs' => []
-            ]
-        ]
-    ]
 );
