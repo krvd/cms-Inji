8441dad51307d573ee50b518ac0cd29daec11140
Alexey Krupsky <admin@inji.ru>
2015-11-04 12:24:51 +0700

    prettyPhoto add

---
+++ system/modules/Libs/static/libs/prettyPhoto/libConfig.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'name' => 'prettyPhoto',
+    'version' => '3.1.6',
+    'files' => [
+        'css' => [
+            'css/prettyPhoto.css'
+        ],
+        'js' => [
+            'js/jquery.prettyPhoto.js'
+        ]
+    ]
+];
