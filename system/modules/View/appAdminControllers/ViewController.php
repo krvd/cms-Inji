40509bf34371f31562ba569406bb1642c6b646ba
Alexey Krupsky <admin@inji.ru>
2015-11-04 18:59:44 +0700

    remove description and author from new themes

--- system/modules/View/appAdminControllers/ViewController.php
+++ system/modules/View/appAdminControllers/ViewController.php
@@ -29,8 +29,6 @@ class ViewController extends Controller
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="">
         {HEAD}
     </head>
     <body>
