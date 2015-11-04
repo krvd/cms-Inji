6139b3c8417b89a40428db4af969bba4d2631a0b
Alexey Krupsky <admin@inji.ru>
2015-11-04 15:57:43 +0700

    menu for install

--- system/modules/UserForms/info.php
+++ system/modules/UserForms/info.php
@@ -2,4 +2,13 @@
 
 return array(
     'name' => 'Формы',
+    'menu' => [
+        'appAdmin' => [
+            [
+                'name' => 'Полученные формы',
+                'href' => '/admin/userForms/recive',
+                'childs' => []
+            ]
+        ]
+    ]
 );
