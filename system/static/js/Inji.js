803e52f7d98d740cdc9ed6e3ed5498c282941664
Alexey Krupsky <admin@inji.ru>
2015-11-05 15:47:40 +0700

    add number format function

--- system/static/js/Inji.js
+++ system/static/js/Inji.js
@@ -18,8 +18,7 @@ Inji.prototype.onLoad = function (callback) {
   if (typeof callback == 'function') {
     if (this.loaded) {
       callback();
-    }
-    else {
+    } else {
       this.onLoadCallbacks.push(callback);
     }
   }
@@ -73,16 +72,14 @@ Inji.prototype.loadScripts = function (scripts, key) {
           inji[scripts[key].name].init();
         }
       }
-    }
-    else {
+    } else {
       inji.loadedScripts[scripts[key]] = true;
       console.log('js ' + scripts[key] + ' loaded');
       inji.event('loadScript', scripts[key]);
     }
     if (typeof (scripts[key + 1]) != 'undefined') {
       inji.loadScripts(scripts, key + 1);
-    }
-    else {
+    } else {
       console.log('All scripts loaded');
       inji.startCallbacks();
     }
@@ -93,8 +90,7 @@ Inji.prototype.addScript = function (script, callback) {
   var src = '';
   if (typeof (script.file) != 'undefined') {
     src = script.file;
-  }
-  else {
+  } else {
     src = script;
   }
   if (inji.loadedScripts[src]) {
@@ -137,5 +133,42 @@ Inji.prototype.randomString = function (length) {
 
   return text;
 }
+Inji.prototype.numberFormat = function (number, decimals, dec_point, thousands_sep) {
+  //// Format a number with grouped thousands
+  // 
+  // +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +	 bugfix by: Michael White (http://crestidg.com)
+
+  var i, j, kw, kd, km;
+
+  // input sanitation & defaults
+  if (isNaN(decimals = Math.abs(decimals))) {
+    decimals = 2;
+  }
+  if (dec_point == undefined) {
+    dec_point = ",";
+  }
+  if (thousands_sep == undefined) {
+    thousands_sep = ".";
+  }
+
+  i = parseInt(number = (+number || 0).toFixed(decimals)) + "";
+
+  if ((j = i.length) > 3) {
+    j = j % 3;
+  } else {
+    j = 0;
+  }
+
+  km = (j ? i.substr(0, j) + thousands_sep : "");
+  kw = i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands_sep);
+  //kd = (decimals ? dec_point + Math.abs(number - i).toFixed(decimals).slice(2) : "");
+  kd = (decimals ? dec_point + Math.abs(number - i).toFixed(decimals).replace(/-/, 0).slice(2) : "");
+
+
+  return km + kw + kd;
+}
+
 var inji = new Inji();
 
