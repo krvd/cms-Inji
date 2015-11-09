d2cc0735d9601240a9b5ae399efb23395c1d0162
Alexey Krupsky <admin@inji.ru>
2015-11-09 13:42:07 +0700

    fix no image

--- system/modules/Merchants/appControllers/content/pay.php
+++ system/modules/Merchants/appControllers/content/pay.php
@@ -5,7 +5,7 @@
       ?>
       <div class="col-sm-2 text-center">
         <a href="/merchants/go/<?= $pay_id; ?>/<?= $merchant->id; ?>">
-          <img src="<?= $merchant->image->path; ?>" class="img-responsive" />
+          <img src="<?= $merchant->image ? $merchant->image->path : '/static/system/images/no-image.png'; ?>" class="img-responsive" />
           <h4><?= $merchant->name; ?></h4>
         </a>
       </div>
