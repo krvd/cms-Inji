462ba8bca106b956f54a44dd156b069392b2c56d
Alexey Krupsky <admin@inji.ru>
2015-11-12 15:07:10 +0700

    add typehead for bootstrap 3

---
+++ system/modules/Libs/static/libs/typeahead/libConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'name' => 'Bootstrap-3-Typeahead',
+    'version' => '3.1.1',
+    'files' => [
+        'js' => [
+            'js/bootstrap3-typeahead.min.js'
+        ]
+    ]
+];
