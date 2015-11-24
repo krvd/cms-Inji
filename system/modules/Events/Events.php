8ebcd1e28217e9a32da40d3b0ba1a14ba3829f7c
Alexey Krupsky <admin@inji.ru>
2015-11-24 21:37:24 +0700

    events. just for info now

---
+++ system/modules/Events/Events.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Item name
+ *
+ * Info
+ *
+ * @author Alexey Krupskiy <admin@inji.ru>
+ * @link http://inji.ru/
+ * @copyright 2015 Alexey Krupskiy
+ * @license https://github.com/injitools/cms-Inji/blob/master/LICENSE
+ */
+
+/**
+ * Description of Events
+ *
+ * @author inji
+ */
+class Events extends Module
+{
+    //put your code here
+}
---
+++ system/modules/Events/models/Event.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Item name
+ *
+ * Info
+ *
+ * @author Alexey Krupskiy <admin@inji.ru>
+ * @link http://inji.ru/
+ * @copyright 2015 Alexey Krupskiy
+ * @license https://github.com/injitools/cms-Inji/blob/master/LICENSE
+ */
+
+namespace Events;
+
+class Event extends \Model
+{
+    static $objectName = 'Событие системы';
+    static $labels = [
+        'name' => 'Название',
+        'event'=>'Событие'
+    ];
+    static $storage = ['type' => 'moduleConfig'];
+    static $cols = [
+        'name' => ['type' => 'text'],
+        'event' => ['type' => 'text'],
+    ];
+    static $dataManagers = [
+        'manager' => [
+            'cols' => [
+                'name','event'
+            ],
+        ]
+    ];
+    static $forms = [
+        'manager' => [
+            'map' => [
+                ['name','event'],
+            ]
+        ]
+    ];
+
+    static function index()
+    {
+        return 'id';
+    }
+
+}
