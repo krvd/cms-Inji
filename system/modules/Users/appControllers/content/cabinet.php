f0e42fd753f75b3a8106c268a0f87211f9251520
Alexey Krupsky <admin@inji.ru>
2015-11-27 12:15:42 +0700

    add classes for user cabinet

--- system/modules/Users/appControllers/content/cabinet.php
+++ system/modules/Users/appControllers/content/cabinet.php
@@ -1,36 +1,38 @@
-<h2>Личный кабинет</h2>
-<div class="row">
-  <div class="col-sm-3">
-    <ul class="nav nav-pills nav-stacked">
-      <?php
-      foreach ($sections as $sectionName => $section) {
-          if (!empty($section['fullWidget']) || !empty($section['href'])) {
-              ?>
-              <li role="presentation" <?= $activeSection == $sectionName ? 'class="active"' : ''; ?>>
-                <a href="<?= !empty($section['href']) ? $section['href'] : "/users/cabinet/{$sectionName}"; ?>"><?= $section['name']; ?></a>
-              </li>
-              <?php
-          }
-      }
-      ?>
-      <li role="presentation">
-        <a href="?logout">Выход</a>
-      </li>
-    </ul>
-  </div>
-  <div class="col-sm-9">
-    <?php
-    if (empty($activeSection) || empty($sections[$activeSection]['fullWidget'])) {
-        foreach ($sections as $section) {
-            if (!empty($section['smallWidget'])) {
+<div class="users-cabinet">
+  <h2>Добро Пожаловать в личный кабинет, <?= \Users\User::$cur->name(); ?></h2>
+  <div class = "row">
+    <div class = "col-sm-3">
+      <ul class = "nav nav-pills nav-stacked">
+        <?php
+        foreach ($sections as $sectionName => $section) {
+            if (!empty($section['fullWidget']) || !empty($section['href'])) {
                 ?>
-                <div class="col-sm-4"><?= $this->widget($section['smallWidget']); ?></div>
+                <li role="presentation" <?= $activeSection == $sectionName ? 'class="active"' : ''; ?>>
+                  <a href="<?= !empty($section['href']) ? $section['href'] : "/users/cabinet/{$sectionName}"; ?>"><?= $section['name']; ?></a>
+                </li>
                 <?php
             }
         }
-    } else {
-        $this->widget($sections[$activeSection]['fullWidget']);
-    }
-    ?>
+        ?>
+        <li role="presentation">
+          <a href="?logout">Выход</a>
+        </li>
+      </ul>
+    </div>
+    <div class="col-sm-9">
+      <?php
+      if (empty($activeSection) || empty($sections[$activeSection]['fullWidget'])) {
+          foreach ($sections as $section) {
+              if (!empty($section['smallWidget'])) {
+                  ?>
+                  <div class="col-sm-4"><?= $this->widget($section['smallWidget']); ?></div>
+                  <?php
+              }
+          }
+      } else {
+          $this->widget($sections[$activeSection]['fullWidget']);
+      }
+      ?>
+    </div>
   </div>
 </div>
