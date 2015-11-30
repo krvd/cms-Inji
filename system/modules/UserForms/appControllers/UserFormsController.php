824a66b0304a03ca23cd01521046eaf909572aec
Alexey Krupsky <admin@inji.ru>
2015-11-30 16:52:26 +0700

    generate form as form class

--- system/modules/UserForms/appControllers/UserFormsController.php
+++ system/modules/UserForms/appControllers/UserFormsController.php
@@ -2,37 +2,20 @@
 
 class UserFormsController extends Controller
 {
-    function getFormHtmlAction($form_id)
+    function getFormHtmlAction($userForm_id)
     {
-        $form = \UserForms\Form::get((int) $form_id);
-        if (!$form) {
+        $userForm = \UserForms\Form::get((int) $userForm_id);
+        if (!$userForm) {
             echo('form not found');
             return;
         }
-
-        echo '<form method = "POST" action = "">';
-        if ($form->description) {
-            echo "<p class = 'text-center'>{$form->description}</p>";
+        $form = new Ui\Form();
+        $form->begin();
+        if ($userForm->description) {
+            echo "<p class = 'text-center'>{$userForm->description}</p>";
         }
-        foreach ($form->inputs as $input) {
-            switch ($input->type) {
-                case 'text':
-                    ?>
-                    <div class ='form-group'>
-                      <label><?= $input->label; ?></label>
-                      <input class ='form-control' type ='text' name ='UserForms[<?= (int) $form_id; ?>][input<?= $input->id; ?>]' required />
-                    </div>
-                    <?php
-                    break;
-                case 'textarea':
-                    ?>
-                    <div class ='form-group'>
-                      <label><?= $input->label; ?></label>
-                      <textarea class ='form-control' name ='UserForms[<?= (int) $form_id; ?>][input<?= $input->id; ?>]' required /></textarea>
-                    </div>
-                    <?php
-                    break;
-            }
+        foreach ($userForm->inputs as $input) {
+            $form->input($input->type, 'UserForms[' . (int) $userForm_id . '][input' . $input->id . ']', $input->label, ['required' => $input->required]);
         }
         ?>
         <button class = 'btn btn-success btn-block'>Отправить</button>
