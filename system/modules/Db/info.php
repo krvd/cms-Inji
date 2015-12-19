750778cb729572fba9f303656e4c0aed2aade5b9
Alexey Krupsky <admin@inji.ru>
2015-12-19 14:14:01 +0700

    return db select for app configre

--- system/modules/Db/info.php
+++ system/modules/Db/info.php
@@ -1,5 +1,13 @@
 <?php
 
 return [
-    'name' => 'Базы данных'
+    'name' => 'Базы данных',
+    'configure' => [
+        'default' => [
+            'type' => 'select',
+            'label' => 'Используемая база данных',
+            'model' => 'Db\Options',
+            'col' => 'connect_alias'
+        ]
+    ]
 ];
