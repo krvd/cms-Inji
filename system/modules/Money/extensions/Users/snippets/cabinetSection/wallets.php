cdedf75472103d258f031f7bc88a530c362f4b5c
Alexey Krupsky <admin@inji.ru>
2015-12-21 14:37:09 +0700

    fix empty wallets

--- system/modules/Money/extensions/Users/snippets/cabinetSection/wallets.php
+++ system/modules/Money/extensions/Users/snippets/cabinetSection/wallets.php
@@ -1,5 +1,9 @@
 <?php
 
+$currencies = Money\Currency::getList(['where' => ['wallet', 1]]);
+if (!$currencies) {
+    return false;
+}
 return [
     'name' => 'Мои кошельки',
     'fullWidget' => 'Money\cabinet/wallets',
