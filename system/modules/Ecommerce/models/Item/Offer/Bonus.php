88970780b0aab6180c62635618ec72ba1c1c66e1
Пользователь <>
2015-12-28 15:02:11 +0700

    Ограничение количества бонусов

--- system/modules/Ecommerce/models/Item/Offer/Bonus.php
+++ system/modules/Ecommerce/models/Item/Offer/Bonus.php
@@ -13,5 +13,26 @@ namespace Ecommerce\Item\Offer;
 
 class Bonus extends \Model
 {
-    
+    static $cols = [
+        'item_offer_id' => ['type' => 'select', 'source' => 'relation', 'relation' => 'offer'],
+        'type' => ['type' => 'text'],
+        'value' => ['type' => 'text'],
+        'count' => ['type' => 'decinal'],
+        'limited' => ['type' => 'bool'],
+        'left' => ['type' => 'number']
+    ];
+
+    static function relations()
+    {
+        return [
+            'offer' => [
+                'model' => 'Ecommerce\Item\Offer',
+                'col' => 'item_offer_id'
+            ]
+        ];
+    }
+
+
 }
+
+
