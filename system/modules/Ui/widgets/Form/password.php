12a3a6d044364cf30d7718ae3b57a23c4c495dfc
Alexey Krupsky <admin@inji.ru>
2016-01-08 12:24:48 +0700

    fix password placeholder

--- system/modules/Ui/widgets/Form/password.php
+++ system/modules/Ui/widgets/Form/password.php
@@ -1,15 +1,13 @@
-
 <?php
 echo empty($options['noContainer']) ? '<div class="form-group">' : '';
 echo $label !== false ? "<label>{$label}" . (!empty($options['required']) ? ' <span class="required-star">*</span>' : '') . "</label>" : '';
 ?>
 <input <?= !empty($options['required']) ? 'required' : ''; ?> 
   type ="password" 
-  placeholder="Повторите пароль" 
+  placeholder="<?= !empty($options['placeholder']) ? $options['placeholder'] : ''; ?>" 
   class="form-control" 
   name = '<?= $name; ?>' 
   />
 <?php
 echo!empty($options['helpText']) ? "<div class='help-block'>{$options['helpText']}</div>" : '';
 echo empty($options['noContainer']) ? '</div>' : '';
-?>
