68a9716ccb7f31267d8b8adf4e51cb142c5e0a5d
Alexey Krupsky <admin@inji.ru>
2016-02-04 10:25:08 +0700

    clear

--- system/modules/Dashboard/install_script.php
+++ system/modules/Dashboard/install_script.php
@@ -1,12 +1,5 @@
 <?php
 
 return function ($step = NULL, $params = []) {
-    App::$cur->db->createTable('dashboard_comment', [
-        'comment_id' => 'pk',
-        'comment_text' => 'text NOT NULL',
-        'comment_user_id' => 'int(11) UNSIGNED  NOT NULL',
-        'comment_model' => 'varchar(255) NOT NULL',
-        'comment_item_id' => 'int(11) UNSIGNED NOT NULL',
-        'comment_date_create' => 'timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP',
-    ]);
+
 };
