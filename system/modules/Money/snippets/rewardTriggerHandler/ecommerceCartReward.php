c662bd3fa0dccbc8b775f2f3e4960a2fdbf1ed8b
Alexey Krupsky <admin@inji.ru>
2016-02-19 13:34:45 +0700

    fix trigger params check

--- system/modules/Money/snippets/rewardTriggerHandler/ecommerceCartReward.php
+++ system/modules/Money/snippets/rewardTriggerHandler/ecommerceCartReward.php
@@ -4,10 +4,12 @@ return [
     'name' => 'Вознаграждение партнерам за закрытие корзины товаров в онлайн магазине',
     'handler' => function($cart, $trigger) {
         $sums = [];
+        $rewardItemExist = empty($trigger->params['item_type_id']);
         foreach ($cart->cartItems as $cartItem) {
-            if (!empty($trigger->params['item_type_id']) && $trigger->params['item_type_id'] != $cartItem->item->type_id) {
+            if (!empty($trigger->params['item_type_id']) && $trigger->params['item_type_id']->value != $cartItem->item->item_type_id) {
                 continue;
             }
+            $rewardItemExist = true;
             $currency_id = $cartItem->price->currency ? $cartItem->price->currency->id : \App::$cur->ecommerce->config['defaultCurrency'];
             if (empty($sums[$currency_id])) {
                 $sums[$currency_id] = $cartItem->final_price * $cartItem->count;
@@ -15,7 +17,11 @@ return [
                 $sums[$currency_id] += $cartItem->final_price * $cartItem->count;
             }
         }
-        App::$cur->money->reward($trigger->reward_id, $sums, $cart->user);
+        var_dump($rewardItemExist, $trigger->_params, $sums);
+        //exit();
+        if ($rewardItemExist) {
+            App::$cur->money->reward($trigger->reward_id, $sums, $cart->user);
+        }
     }
         ];
         
