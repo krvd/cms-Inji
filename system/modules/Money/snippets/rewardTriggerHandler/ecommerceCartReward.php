e8e0c16f389e9502c8a8883c86847eae0010284b
Alexey Krupsky <admin@inji.ru>
2016-03-20 14:04:39 +0700

    improove cart closed reward trigger

--- system/modules/Money/snippets/rewardTriggerHandler/ecommerceCartReward.php
+++ system/modules/Money/snippets/rewardTriggerHandler/ecommerceCartReward.php
@@ -5,21 +5,25 @@ return [
     'handler' => function($cart, $trigger) {
         $sums = [];
         $rewardItemExist = empty($trigger->params['item_type_id']);
+        if (!$rewardItemExist) {
+            $allowTypes = explode(',', $trigger->params['item_type_id']);
+        } else {
+            $allowTypes = [];
+        }
         foreach ($cart->cartItems as $cartItem) {
-            if (!empty($trigger->params['item_type_id']) && $trigger->params['item_type_id']->value != $cartItem->item->item_type_id) {
+            if ($allowTypes && !in_array($cartItem->item->item_type_id, $allowTypes)) {
                 continue;
             }
             $rewardItemExist = true;
             $currency_id = $cartItem->price->currency ? $cartItem->price->currency->id : \App::$cur->ecommerce->config['defaultCurrency'];
             if (empty($sums[$currency_id])) {
-                $sums[$currency_id] = $cartItem->final_price * $cartItem->count;
+                $sums[$currency_id] = $cartItem->price->price * $cartItem->count;
             } else {
-                $sums[$currency_id] += $cartItem->final_price * $cartItem->count;
+                $sums[$currency_id] += $cartItem->price->price * $cartItem->count;
             }
         }
         if ($rewardItemExist) {
             App::$cur->money->reward($trigger->reward_id, $sums, $cart->user);
         }
-    }
-        ];
+    }];
         
