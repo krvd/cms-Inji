361860a9f4d4ddd1400baa10d53a0f13a59928a9
Alexey Krupsky <admin@inji.ru>
2016-03-20 18:17:10 +0700

    ban user, delete msgs

--- system/modules/Chats/static/js/chat.js
+++ system/modules/Chats/static/js/chat.js
@@ -1,15 +1,18 @@
 inji.Chats = {};
 inji.Chats.chats = [];
-inji.Chats.get = function (id) {
-  var chat = $('#' + id);
-  if (chat.data('chats-index') === undefined) {
+inji.Chats.get = function (id, params) {
+  var chatElement = $('#' + id);
+  var chat;
+  if (chatElement.data('chats-index') === undefined) {
     inji.Chats.chats.push({
-      element: chat,
+      element: chatElement,
       timer: null,
-      chatId: chat.data('chat-id'),
-      msgCount: chat.data('msg-count'),
+      chatId: chatElement.data('chat-id'),
+      msgCount: chatElement.data('msg-count'),
       perPage: 20,
-      lastEventDate: chat.data('last-event-date'),
+      lastEventDate: chatElement.data('last-event-date'),
+      reverse: params.reverse ? true : false,
+      scrollable: true,
       members: {},
       checkoutMembers: function () {
         var members = this.members;
@@ -37,13 +40,32 @@ inji.Chats.get = function (id) {
             }
           }
         })
-      }
+      },
+      deleteMsg: function (id, chatId) {
+        inji.Server.request({
+          url: 'chats/deleteMsg/' + id,
+          success: function () {
+            $('#chat' + chatId + 'msg' + id).remove();
+          }
+        });
+      },
+      banUser: function (msgId, chatId) {
+        inji.Server.request({
+          url: 'chats/banUser/' + msgId,
+          success: function (msgIds) {
+            for (var key in msgIds) {
+              $('#chat' + chatId + 'msg' + key).remove();
+            }
+          }
+        });
+      },
     });
     var index = inji.Chats.chats.length - 1
-    chat.data('chats-index', index);
+    chatElement.data('chats-index', index);
     inji.Chats.init(index);
+    return inji.Chats.chats[index];
   }
-  return chat;
+  return inji.Chats.chats[chatElement.data('chats-index')];
 }
 inji.Chats.init = function (chatIndex) {
   inji.Chats.chats[chatIndex].timer = setInterval(function () {
@@ -59,19 +81,41 @@ inji.Chats.init = function (chatIndex) {
           var msg = data.messages[key];
           inji.Chats.chats[chatIndex].lastEventDate = msg.message.chat_message_date_create;
           var template = inji.Chats.chats[chatIndex].element.find('.chats-chat-message-template').html();
-          template = template.replace('message-date_create', msg.message.chat_message_date_create);
-          template = template.replace('user-firstName', msg.userFirstName);
-          template = template.replace('user-fullName', msg.fullUserName);
-          template = template.replace('message-text', msg.message.chat_message_text);
-          inji.Chats.chats[chatIndex].element.find('.chats-chat-messageList').prepend(template);
-        }
-        var i = 0;
-        var allMsgs = inji.Chats.chats[chatIndex].element.find('.chats-chat-messageList>*');
-        while (allMsgs[i]) {
-          if (i > 20) {
-            $(allMsgs[i]).remove();
+          template = template.replace(/message-id/g, msg.message.chat_message_id);
+          template = template.replace(/message-userId/g, msg.message.chat_message_user_id);
+          template = template.replace(/message-date_create/g, msg.message.chat_message_date_create);
+          template = template.replace(/user-firstName/g, msg.userFirstName);
+          template = template.replace(/user-fullName/g, msg.fullUserName);
+          template = template.replace(/user-photo/g, msg.userPhoto);
+          template = template.replace(/message-text/g, msg.message.chat_message_text);
+          var messageList = inji.Chats.chats[chatIndex].element.find('.chats-chat-messageList');
+          if (inji.Chats.chats[chatIndex].reverse) {
+            messageList.append(template);
+            if (inji.Chats.chats[chatIndex].scrollable) {
+              var height = messageList.height() + messageList.scrollTop() + parseInt(messageList.css('paddingTop')) + parseInt(messageList.css('paddingBottom'));
+              messageList.scrollTop(messageList[0].scrollHeight);
+            }
+          } else {
+            messageList.prepend(template);
           }
-          i++;
+        }
+
+        var allMsgs = inji.Chats.chats[chatIndex].element.find('.chats-chat-messageList>*');
+        if (allMsgs.length > 20) {
+          var i = 0;
+          if (inji.Chats.chats[chatIndex].reverse) {
+            var diff = allMsgs.length - 20;
+            while (i <= diff && i >= 0) {
+              $(allMsgs[i]).remove();
+              i--;
+            }
+          } else {
+            i = allMsgs.length + 1;
+            while (i > 20 && allMsgs[i]) {
+              $(allMsgs[i]).remove();
+              i++;
+            }
+          }
         }
       }
     });
@@ -81,7 +125,7 @@ inji.Chats.sendForm = function (form, id) {
   var chat = inji.Chats.get(id);
   var formData = new FormData(form);
   inji.Server.request({
-    url: '/chats/sendForm/' + chat.data('chat-id'),
+    url: '/chats/sendForm/' + chat.element.data('chat-id'),
     type: 'POST',
     data: formData,
     processData: false,
