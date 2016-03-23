c475b9b4c1fc05a8e52e4e52687bdc2ab6153d5d
Alexey Krupsky <admin@inji.ru>
2016-03-23 13:50:29 +0700

    js cookie library

---
+++ system/modules/Libs/objects/JsCookie.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Js-Cookie library
+ *
+ * @author Alexey Krupskiy <admin@inji.ru>
+ * @link http://inji.ru/
+ * @copyright 2015 Alexey Krupskiy
+ * @license https://github.com/injitools/cms-Inji/blob/master/LICENSE
+ */
+
+namespace Libs;
+
+class JsCookie extends \Object
+{
+    public static $name = 'JavaScript Cookie';
+    public static $files = [
+        'js' => [
+            '/static/moduleAsset/libs/libs/js-cookie/js.cookie-2.1.0.min.js',
+        ],
+    ];
+
+}
