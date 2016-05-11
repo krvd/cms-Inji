be2b3302d07be8ce43cfff43a86c0350e1de205a
Alexey Krupsky <admin@inji.ru>
2016-05-11 11:04:20 +0700

    childforms has begin

--- system/modules/Ui/widgets/Form/end.php
+++ system/modules/Ui/widgets/Form/end.php
@@ -1,16 +1,20 @@
 <?php
-if ($btnText !== false) {
+if (empty($params['activeForm']) || $options['activeForm']->parent === null) {
+    if ($btnText !== false) {
+        ?>
+        <div class="form-group">
+          <button class ='btn btn-primary' 
+                  <?php
+                  foreach ($attributs as $attribute => $value) {
+                      echo " {$attribute} = '{$value}' ";
+                  }
+                  ?>
+                  ><?= $btnText; ?></button>
+        </div>
+        <?php
+    }
     ?>
-    <div class="form-group">
-        <button class ='btn btn-primary' 
-                <?php
-                foreach ($attributs as $attribute => $value) {
-                    echo " {$attribute} = '{$value}' ";
-                }
-                ?>
-                ><?= $btnText; ?></button>
-    </div>
+    </form>
     <?php
 }
 ?>
-</form>
