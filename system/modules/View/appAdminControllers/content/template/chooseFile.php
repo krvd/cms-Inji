5b690cf96031e19064cb7be97e9e2eb3b4d9542d
Alexey Krupsky <admin@inji.ru>
2016-05-27 13:41:14 +0700

    edit static templates files

---
+++ system/modules/View/appAdminControllers/content/template/chooseFile.php
@@ -0,0 +1,11 @@
+<h1>Файлы темы оформления <?= $template->config['template_name']; ?></h1>
+<a href ='/admin/view/template/editFile/<?= $template->name; ?>?path=<?= $template->config['file']; ?>'>Основной файл шаблона</a>
+<hr/>
+<?php
+foreach ($template->config['css'] as $file) {
+    if (file_exists($template->path . '/css/' . $file)) {
+        ?>
+        <a href ='/admin/view/template/editFile/<?= $template->name; ?>?path=<?= 'css/' . $file; ?>'><?= $file; ?></a>
+        <?php
+    }
+}
--- system/modules/View/appAdminControllers/content/template/edit.php
+++ system/modules/View/appAdminControllers/content/template/edit.php
@@ -1,2 +1,17 @@
-<?php
-
+<h2><?= $_GET['path']; ?></h2>
+<div id="editor" style="height:500px"><?= htmlspecialchars($content); ?></div>
+<button class="btn btn-primary" onclick="saveCode()">Сохранить</button>
+<script>
+    var editor;
+    function saveCode() {
+      if (editor) {
+        inji.Server.request({
+          data: editor.getValue(),
+          method: 'POST'
+        });
+      }
+    }
+    inji.onLoad(function () {
+      editor = ace.edit("editor");
+    });
+</script>
